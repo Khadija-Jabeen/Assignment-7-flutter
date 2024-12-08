90aa1dd07c1c5f96c7efa6f9ca2e62ca3938496a
Khadija-Jabeen <khadijajabeen2004@gmail.com>
2024-12-08 10:32:07 +0500

    login signup

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:signup_login/firebase_options.dart';
+
+import 'login.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(), // SplashScreen as the initial screen
+    );
+  }
+}
+
